8cc2e6664f480b092ca477f3aa5d8ebc974ea493
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-09-18 13:14:02 +0300

    Improve validation
    Signed-off-by: George Ndirangu <ndirangu.mepawa@gmail.com>

--- search/musicbrainz/utils.py
+++ search/musicbrainz/utils.py
@@ -12,35 +12,28 @@ def validate_result(result, fragment, fragments):
     album = ""
 
     for recording in result['recording-list']:
-        if int(recording['ext:score']) > score_cache:
-            score_cache = int(recording['ext:score'])
-            recording_to_return = recording
+        title = recording["title"].lower()
+        artist = recording["artist-credit"][0]["artist"]["name"].lower()
+        album = recording['release-list'][0]['title'].lower()
+        recording_score = int(recording['ext:score'])
 
-            title = recording["title"].lower()
-            artist = recording["artist-credit"][0]["artist"]["name"].lower()
-            album = recording['release-list'][0]['title'].lower()
+        if title in fragments:
+            if recording_score > score_cache:
+                score_cache = recording_score
+                recording_to_return = recording
 
-            if score_cache == 100:
-                print("found score 100")
-                # TODO check if the artist and album match any entry in fragments
-                if title in fragments:
+                if score_cache == 100:
+                    print("found score 100")
                     break
+        else:
+            logging.warning(f"Didn't find title {title} in fragments. Skipping to next iteration")
+            continue
 
     if score_cache < 90:
         recording_to_return = None
         found_valid = False
-        logging.warning("score was less than 90")
-
-    if title in fragments:
-        pass
-    else:
-        recording_to_return = None
-        found_valid = False
-        logging.warning(f"Could not find the the returned title, album or artist in the filename fragmments and so "
-                        f"cannot validate this result though score is {score_cache} \n Fragment: {fragment} \n"
-                        f" Result: title: {title} artist: {artist} album:{album}")
-
-    if score_cache < 100:
+        logging.warning("Score after iterations was less than 90.Cant return this as valid result. Bailing...")
+    elif 90 <= score_cache < 100:
         logging.warning(f"settling for less score: {score_cache}")
 
     return found_valid, recording_to_return
