93a7e802a33faeb4c3a8a194c40ac646ec7888fe
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-09-19 19:07:29 +0300

    Reduce false positives associated with short titles
    Signed-off-by: George Ndirangu <ndirangu.mepawa@gmail.com>

--- search/musicbrainz/utils/check_match.py
+++ search/musicbrainz/utils/check_match.py
@@ -26,88 +26,77 @@ def check_title_match(title, fragments, threshold):
             logging.warning(f"Dint find fragment {title_fragment} in title_fragments")
     return title_does_match
 
-# def check_artist_match(recording, fragments):
-#     artist = ""
-#     artist_does_match = False
-#     matches_found = 0
-#
-#     try:
-#         artist = recording["artist-credit"][0]["artist"]["name"].lower()
-#
-#         # IF at least 2 fragments in artist_fragments are present in the fragments
-#         artist = cleanup(artist)
-#         artist_fragments = set(artist.split(" "))
-#
-#
-#         for artist_fragment in artist_fragments:
-#             if artist_fragment in fragments:
-#                 matches_found += 1
-#                 logging.warning(f"match {matches_found} {artist_fragment} in artist_fragments")
-#
-#                 if matches_found >= 2:
-#                     artist_does_match = True
-#                     break
-#
-#             else:
-#                 logging.warning(f"Dint find fragment {artist_fragment} in title_fragments")
-#
-#     except KeyError as key_error:
-#         logging.warning(f"Key {key_error.args} not found. Can't validate with artist")
-#
-#     return artist_does_match, matches_found
-#
-#
-# def check_album_match(recording, fragments):
-#     album = ""
-#     album_does_match = False
-#     matches_found = 0
-#
-#     try:
-#         album = recording["release-list"][0]['title'].lower()
-#
-#         # IF at least 2 fragments in album_fragments are present in the fragments
-#         album = cleanup(album)
-#         album_fragments = set(album.split(" "))
-#
-#
-#         for album_fragment in album_fragments:
-#             if album_fragment in fragments:
-#                 matches_found += 1
-#                 logging.warning(f"match {matches_found} {album_fragment} in album_fragments")
-#
-#                 if matches_found >= 2:
-#                     album_does_match = True
-#                     break
-#
-#             else:
-#                 logging.warning(f"Dint find fragment {album_fragment} in title_fragments")
-#
-#     except KeyError as key_error:
-#         logging.warning(f"Key {key_error.args} not found. Can't validate with album name")
-#
-#     return album_does_match, matches_found
-#
-#
-# def check_album_artist_title_match(recording, fragments):
-#     title = recording["title"].lower()
-#
-#     title_matches_fragments, matches_found_title = check_title_match(title, fragments, threshold=2)
-#     artist_matches_fragments, matches_found_artist = check_artist_match(recording, fragments)
-#     album_matches_fragments, matches_found_album = check_album_match(recording, fragments)
-#
-#     if artist_matches_fragments:
-#         matches_found = 0
-#         if matches_found_title > matches_found_artist:
-#             matches_found = matches_found_title
-#         else:
-#             matches_found = matches_found_artist
-#         return (title_matches_fragments and artist_matches_fragments), matches_found
-#     elif album_matches_fragments:
-#         matches_found = 0
-#         if matches_found_title > matches_found_album:
-#             matches_found = matches_found_title
-#         else:
-#             matches_found = matches_found_album
-#         return (title_matches_fragments and album_matches_fragments), matches_found
-#     else:
-#         return False, 0
+
+def check_artist_match(recording, fragments):
+    artist = ""
+    artist_does_match = False
+    matches_found = 0
+
+    try:
+        artist = recording["artist-credit"][0]["artist"]["name"].lower()
+
+        # IF at least 2 fragments in artist_fragments are present in the fragments
+        artist = cleanup(artist)
+        artist_fragments = set(artist.split(" "))
+
+        for artist_fragment in artist_fragments:
+            if artist_fragment in fragments:
+                matches_found += 1
+                logging.warning(f"match {matches_found} {artist_fragment} in artist_fragments")
+
+                if matches_found >= 2:
+                    artist_does_match = True
+                    break
+
+            else:
+                logging.warning(f"Dint find fragment {artist_fragment} in title_fragments")
+
+    except KeyError as key_error:
+        logging.warning(f"Key {key_error.args} not found. Can't validate with artist")
+
+    return artist_does_match, matches_found
+
+
+def check_album_match(recording, fragments):
+    album = ""
+    album_does_match = False
+    matches_found = 0
+
+    try:
+        album = recording["release-list"][0]['title'].lower()
+
+        # IF at least 2 fragments in album_fragments are present in the fragments
+        album = cleanup(album)
+        album_fragments = set(album.split(" "))
+
+        for album_fragment in album_fragments:
+            if album_fragment in fragments:
+                matches_found += 1
+                logging.warning(f"match {matches_found} {album_fragment} in album_fragments")
+
+                if matches_found >= 2:
+                    album_does_match = True
+                    break
+
+            else:
+                logging.warning(f"Dint find fragment {album_fragment} in title_fragments")
+
+    except KeyError as key_error:
+        logging.warning(f"Key {key_error.args} not found. Can't validate with album name")
+
+    return album_does_match, matches_found
+
+
+def check_album_artist_title_match(recording, fragments):
+    title = recording["title"].lower()
+
+    title_matches_fragments = check_title_match(title, fragments, threshold=1)
+    artist_matches_fragments, matches_found_artist = check_artist_match(recording, fragments)
+    album_matches_fragments, matches_found_album = check_album_match(recording, fragments)
+
+    if title_matches_fragments and (matches_found_album + matches_found_artist >= 2):
+        logging.warning(
+            f"Yaay... artist_matches_fragments: {matches_found_artist} album_matches_fragments: {matches_found_album} title: {title_matches_fragments} {title}")
+        return True
+    else:
+        return False
--- search/musicbrainz/utils/validate_result.py
+++ search/musicbrainz/utils/validate_result.py
@@ -1,6 +1,6 @@
 import logging
 
-from search.musicbrainz.utils.check_match import check_title_match
+from search.musicbrainz.utils.check_match import check_title_match, check_album_artist_title_match
 
 
 def validate_result(result, fragment, fragments):
@@ -15,17 +15,31 @@ def validate_result(result, fragment, fragments):
             title = recording["title"].lower()
             recording_score = int(recording['ext:score'])
 
-            if title in fragments or check_title_match(title, fragments, threshold=3):
-                if recording_score >= score_cache:
-                    score_cache = recording_score
-                    recording_to_return = recording
+            if len(title.split(' ')) >= 3:
+                if title in fragments or check_title_match(title, fragments, threshold=3):
+                    if recording_score >= score_cache:
+                        score_cache = recording_score
+                        recording_to_return = recording
 
-                    if score_cache == 100:
-                        print("found score 100")
-                        break
+                        if score_cache == 100:
+                            print("found score 100")
+                            break
+                else:
+                    logging.warning(f"Couldn't validate title {title} against fragments. Skipping to next iteration")
+                    continue
+
             else:
-                logging.warning(f"Couldn't validate title {title} against fragments. Skipping to next iteration")
-                continue
+                if check_album_artist_title_match(recording, fragments):
+                    if recording_score >= score_cache:
+                        score_cache = recording_score
+                        recording_to_return = recording
+
+                        if score_cache == 100:
+                            print("found score 100")
+                            break
+                else:
+                    logging.warning(f"Couldn't validate title {title} using second method. Skipping to next iteration")
+                    continue
 
     except KeyError as key_error:
         logging.warning(f"Key {key_error.args} not found.Validation failed  so move on to next iteration.")
